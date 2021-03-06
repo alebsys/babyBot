88b9755db36f9189ebcd939f2b1267ff00300981
Aleksandr Lebedev <lebedevam88@gmail.com>
2021-03-06 17:52:44 +0300

    Define validationDateTests struct into func TestValidationDate

--- pkg/telegram/handlers_test.go
+++ pkg/telegram/handlers_test.go
@@ -2,20 +2,17 @@ package telegram
 
 import "testing"
 
-type validationDateTest struct {
-	name     string
-	date     string
-	expected string
-}
-
-var validationDateTests = []validationDateTest{
-	{"Sanity", "02/01/06", ""},
-	{"With error", "02-01-06", "error from validationDate"},
-	{"Empty", "", "error from validationDate"},
-	{"Future", "02/01/55", "error from future"},
-}
-
 func TestValidationDate(t *testing.T) {
+	var validationDateTests = []struct {
+		name     string
+		date     string
+		expected string
+	}{
+		{"Sanity", "02/01/06", ""},
+		{"With error", "02-01-06", "problem parsing date"},
+		{"Empty", "", "problem parsing date"},
+		{"Future", "02/01/55", "error from future"},
+	}
 	for _, test := range validationDateTests {
 		err := validationDate(test.date)
 		if err != nil && err.Error() != test.expected {
